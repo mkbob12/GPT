ec39b337002ce8ef4109811592c41717a38508fe
soooochan <nowee018@naver.com>
2023-07-24 01:27:21 +0900

    add : FLBA: ;

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string> 
 #include <stdint.h> // uint8_t 타입을 사용하기 위해 추가 
+#include <vector>
 
 using namespace std;
 
@@ -11,29 +12,59 @@ int main(){
     string filename="gpt_128.dd";
     file.open(filename, ios::binary);
 
-    if (!file.is_open()){
-        printf("%s","error opening the file");
-    }
-
     /*
         reinterpret_cast는 형변환 연산자로 
     */
     //move the file pointer forward by 400 bytes 
 
     file.seekg(1024,ios::beg);
-
-    uint8_t number;
     int i = 0;
     int f = 0;
+    uint8_t number;
+
+
     while(file.read(reinterpret_cast<char*>(&number), sizeof(number))){
         
-        printf("%0x",number);
-        if(i == 50){
+        
+        if(i >= 0 & i <16){
+            if( i == 0){
+                printf("%s","\nPartition Type GUID\n");
+            }
+            printf("%02x ",number);
+           
+        }
+
+        if(i >= 16 && i < 32){
+            if( i ==  16){
+                printf("%s","\nUnique Parition GUID\n");
+            }
+            printf("%02x ",number);
+        }
+
+        uint8_t hexValues[8];
+        if(i >= 32 && i < 40){
+            if( i ==  32){
+                printf("%s","\nFLBA\n");
+            }
+            printf("%02x ",number);
+            hexValues[i-32] = number;
+        }
+
+        if (i == 40){
+            for(int j = 0; j < 8; j++){
+                printf("%02x ",hexValues[j]);
+            }
+        }
+        
+
+        if ( i == 40 ){
             break;
         }
+
+       
+       
         i++;
     }
-
 
     file.close();
     return -1;
