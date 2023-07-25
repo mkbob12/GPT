08b11a9bc0b7f834eba4300690f186facdaa6334
soooochan <nowee018@naver.com>
2023-07-25 16:59:59 +0900

    file sytstem 정보 출력

--- main.cpp
+++ main.cpp
@@ -3,70 +3,136 @@
 #include <string> 
 #include <stdint.h> // uint8_t 타입을 사용하기 위해 추가 
 #include <vector>
+#include <cstdint> // static_cast
+#include <iomanip>
+#include <codecvt>        // std::codecvt_utf8
 
 using namespace std;
 
 
-int main(){
+
+
+
+// === 초기화 
+
+
+int i = 0;
+int f = 0;
+int j = 0; 
+
+
+int file(int x){
     ifstream file;
     string filename="gpt_128.dd";
     file.open(filename, ios::binary);
+    cout<<"x" << x <<endl;
+    file.seekg(x,ios::beg);
 
-    /*
-        reinterpret_cast는 형변환 연산자로 
-    */
-    //move the file pointer forward by 400 bytes 
-
-    file.seekg(1024,ios::beg);
-    int i = 0;
-    int f = 0;
-    uint8_t number;
-
-
-    while(file.read(reinterpret_cast<char*>(&number), sizeof(number))){
-        
-        
-        if(i >= 0 & i <16){
+    uint16_t number;
+    uint32_t bit; 
+    bool type  = true;
+    while(file.read(reinterpret_cast<char *>(&number), sizeof(number))){
+       
+        if(i >= 0 & i <8){
             if( i == 0){
+                printf("===================================");
                 printf("%s","\nPartition Type GUID\n");
+                
             }
-            printf("%02x ",number);
-           
+            printf("%04x ",number);
         }
-
-        if(i >= 16 && i < 32){
-            if( i ==  16){
+        
+        if(i >= 8 && i < 16){
+            if( i ==  8){
                 printf("%s","\nUnique Parition GUID\n");
             }
-            printf("%02x ",number);
+            printf("%04x ",number);
         }
 
-        uint8_t hexValues[8];
-        if(i >= 32 && i < 40){
-            if( i ==  32){
-                printf("%s","\nFLBA\n");
-            }
-            printf("%02x ",number);
-            hexValues[i-32] = number;
-        }
+        if(i == 16){
+            
+            ifstream file1;
+            file1.open(filename, ios::binary);
+            x = x + 32;
+            file1.seekg(x,ios::beg);
+            uint64_t number64;
+            uint64_t FLBA;
+            uint64_t LLBA;
+            uint64_t size;
+            j = 0;
+            string utf8String;
+            while(file1.read(reinterpret_cast<char *>(&number64), sizeof(number64))){
+                if (j == 0){
+                    printf("%s","\nFLBA ");
+                    printf("%016lx ", number64);
+                    FLBA = number64;
+                
+                };
+               
+                if( j == 1){
+                    printf("%s","\nLLBA ");
+                    printf("%016lx\n",number64);
+                    LLBA = number64;
+                    size = (LLBA - FLBA) * 512;
+                    printf("size %ld\n",size);
+                    printf("real_address  %ld\n", FLBA * 512);
+                    file.seekg(LLBA * 512 + 3 ,ios::beg);
+                    file.read(reinterpret_cast<char *>(&number64), sizeof(number64));
+                    
+                    
+                    // Convert the number to a string
+                    for (int i = 7; i >=0; --i) {
+                        unsigned char byte = (number64 >> (i * 8)) & 0xFF;
+                        utf8String.push_back(byte);
+                    }
+                    string reverseString; 
+                    for (int i = utf8String.length() -1; i >=0; i--) {
+                        reverseString.push_back(utf8String[i]);
+                    }
 
-        if (i == 40){
-            for(int j = 0; j < 8; j++){
-                printf("%02x ",hexValues[j]);
-            }
-        }
-        
-
-        if ( i == 40 ){
+                    cout << " " << endl;
+                    cout << "Decoded UTF-8 : ";
+                    cout <<  reverseString << endl;
+                    printf("=================================== \n");
+                    i = -1; 
+                    j = 0; 
+                    type = false;
+                    file1.close();
+                    break;
+                };
+                j++;
+            };
+            
+        };    
+        i++;
+        if(type == false){
             break;
         }
-
-       
-       
-        i++;
+        
     }
 
+            
     file.close();
-    return -1;
+    return 1;
+       
+
 
 }
+     
+  
+int main(){
+   int x = 1024;
+   
+   for (int k =0 ; k < 2; k++){
+     int judge;
+    judge = file(x);
+    
+    x += 128;
+    
+   }
+    
+   
+
+
+   }
+
